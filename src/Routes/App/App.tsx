5a07b80d809e302ddca940eb4b3c9909b35e15f1
KrishnaKapadia <kapadikris@myvuw.ac.nz>
2018-11-09 20:55:52 +1300

    Initial layout

--- src/Routes/App/App.tsx
+++ src/Routes/App/App.tsx
@@ -9,33 +9,44 @@ class App extends React.Component<IProps> {
       <div className="App">
         <div class="bg" />
 
-        <header><a href="#">ZoomZoom</a></header>
-
         <main>
-          <section class="card">
-            <ul>
-              <li>
-                <span></span>
-                <strong>How may I help you?</strong>
-              </li>
-              <li>
-                <span></span>
-                <strong>How may I help you?</strong>
-              </li>
-              <li>
-                <span></span>
-                <strong>How may I help you?</strong>
-              </li>
-            </ul>
-          </section>
-
           <section class="primary">
-            <h1>Your Personal Assistant</h1>
-            <p>Get builders fast</p>
+            <section class="title">
+              <h1>Bit Casino</h1>
+              <p>Play for a chance to win!</p>
+            </section>
 
-            <a href="#">Check it out!</a>
+              <section class="card">
+                <h1 class="card-title">Start Playing!</h1>
+
+                <section class="inner">
+                  <h2>1. Select Amount</h2>
+                  <ul>
+                    <li class="button">1.0 WEI</li>
+                    <li class="button">5.0 WEI</li>
+                    <li>OR</li>
+                    <li>Custom Amount</li>
+                    <li class="input"><input type="text" placeholder="15.0 WEI"/></li>
+                  </ul>
+                </section>
+
+                <br />
+
+                <section class="inner line-top">
+                  <h2>2. Select Bet</h2>
+                  <div style={{ marginBottom: '10px', width: '100%' }}>
+                    <button>10</button>
+                    <button>10-15</button>
+                    <button>15</button>
+                  </div>
+                </section>
+
+                <div class="submit-button"><h3>Submit</h3></div>
+              </section>
           </section>
+
         </main>
+
       </div>
     );
   }
